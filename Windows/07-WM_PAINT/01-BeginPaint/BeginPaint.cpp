56bb773d8dd2d72b7d976cb9048cfb30c8e6cd5a
yashPat98 <prime.ash75@gmail.com>
2020-07-06 22:17:03 +0530

    Changes in WM_PAINT Assignment and uploading fullscreen assignment

--- Windows/07-WM_PAINT/01-BeginPaint/BeginPaint.cpp
+++ Windows/07-WM_PAINT/01-BeginPaint/BeginPaint.cpp
@@ -1,6 +1,10 @@
 //headers
 #include <windows.h>
 
+//macros
+#define WIDTH  800
+#define HEIGHT 600
+
 //function declaration
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -12,33 +16,38 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
     HWND hwnd;
     MSG msg;
     TCHAR szAppName[] = TEXT("MyApp");
+    int xCord, yCord;
 
     //code
     //initialization of WNDCLASSEX
-    wndclass.cbSize = sizeof(WNDCLASSEX);
-    wndclass.style = CS_HREDRAW | CS_VREDRAW;
-    wndclass.cbClsExtra = 0;
-    wndclass.cbWndExtra = 0;
-    wndclass.lpfnWndProc = WndProc;
-    wndclass.hInstance = hInstance;
-    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
-    wndclass.lpszClassName = szAppName;
-    wndclass.lpszMenuName = NULL;
-    wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.cbSize         = sizeof(WNDCLASSEX);
+    wndclass.style          = CS_HREDRAW | CS_VREDRAW;
+    wndclass.cbClsExtra     = 0;
+    wndclass.cbWndExtra     = 0;
+    wndclass.lpfnWndProc    = WndProc;
+    wndclass.hInstance      = hInstance;
+    wndclass.hIcon          = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor        = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground  = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wndclass.lpszClassName  = szAppName;
+    wndclass.lpszMenuName   = NULL;
+    wndclass.hIconSm        = LoadIcon(NULL, IDI_APPLICATION);
 
     //register above class
     RegisterClassEx(&wndclass);
+
+    //get x and y coordinates for window (left-top)
+    xCord = ((GetSystemMetrics(SM_CXSCREEN) - WIDTH) / 2);
+    yCord = ((GetSystemMetrics(SM_CYSCREEN) - HEIGHT) / 2);
 
     //create window
     hwnd = CreateWindow(szAppName,
         TEXT("My Application : WM_PAINT"),
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
+        xCord,
+        yCord,
+        WIDTH,
+        HEIGHT,
         NULL,
         NULL,
         hInstance,
