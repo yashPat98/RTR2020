cdc0891e083df5a55ab604b2c076eba4b959c0d3
yashPat98 <prime.ash75@gmail.com>
2020-09-07 16:57:03 +0530

    Uploading Assignment Output

---
+++ Windows/36-DeathlyHallowsAnimation/DeathlyHallowsAnimation.cpp
@@ -0,0 +1,481 @@
+//headers
+#include <windows.h>
+#include <stdio.h>
+#include <gl/gl.h>
+#include <gl/GLU.h>
+#include <math.h>
+#include "RESOURCES.h"
+
+#pragma comment(lib, "OpenGL32.lib")
+#pragma comment(lib, "glu32.lib")
+
+
+//macros
+#define WIN_WIDTH  800                                  //window width
+#define WIN_HEIGHT 600                                  //window height
+#define PI         3.141529                             //constant
+
+//global function declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+
+//global variable declaration
+FILE *gpFile = NULL;                                     //log file
+
+bool gbFullscreen = false;                               //toggling fullscreen
+HWND ghwnd = NULL;                                       //global hwnd
+DWORD dwStyle;                                           //window style
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };    //window placement before fullscreen
+
+bool gbActiveWindow = false;                             //render only if window is active
+HDC ghdc = NULL;                                         //current device context
+HGLRC ghrc = NULL;                                       //rendering context
+
+
+//WinMain() - entry point function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+    //function prototypes
+    void Initialize(void);
+    void Display(void);
+
+    //variable declaration
+    WNDCLASSEX wndclass;
+    HWND hwnd;
+    MSG msg;
+    TCHAR szAppName[] = TEXT("MyApp");
+    int cxScreen, cyScreen;
+    bool bDone = false;
+
+    //code
+    //create/open 'log.txt' file
+    if(fopen_s(&gpFile, "log.txt", "w") != 0)
+    {
+        MessageBox(NULL, TEXT("Cannot open 'log.txt' file"), TEXT("Error"), MB_OK | MB_ICONERROR);
+        exit(0);
+    }
+    else
+    {
+        fprintf(gpFile, "'log.txt' file created successfully.\nProgram started successfully.\n\n");
+    }
+
+    //initialization of WNDCLASSEX
+    wndclass.cbSize         = sizeof(WNDCLASSEX);
+    wndclass.style          = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wndclass.lpfnWndProc    = WndProc;
+    wndclass.cbClsExtra     = 0;
+    wndclass.cbWndExtra     = 0;
+    wndclass.hInstance      = hInstance;
+    wndclass.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+    wndclass.hCursor        = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground  = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wndclass.lpszClassName  = szAppName;
+    wndclass.lpszMenuName   = NULL;
+    wndclass.hIconSm        = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+
+    //register above class
+    RegisterClassEx(&wndclass);
+
+    cxScreen = GetSystemMetrics(SM_CXSCREEN);
+    cyScreen = GetSystemMetrics(SM_CYSCREEN);
+
+    //create window
+    hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+        szAppName,
+        TEXT("OpenGL : Deathly Hallows Animation"),
+        WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+        (cxScreen - WIN_WIDTH) / 2,
+        (cyScreen - WIN_HEIGHT) / 2,
+        WIN_WIDTH,
+        WIN_HEIGHT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    ghwnd = hwnd;
+    
+    Initialize();
+
+    //show window
+    ShowWindow(hwnd, iCmdShow);
+    SetForegroundWindow(hwnd);
+    SetFocus(hwnd);
+
+    //game loop
+    while(bDone == false)
+    {
+        if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            if(msg.message == WM_QUIT)
+            {
+                    bDone = true;
+            }
+            else
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+            if(gbActiveWindow == true)
+            {
+                //here you should call update function for OpenGL rendering
+
+                //here you should call display function for OpenGL rendering
+                Display();
+            }
+        }
+        
+    }
+
+    return ((int)msg.wParam);
+}
+
+
+//WndProc() - callback function
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    //function prototypes
+    void ToggleFullscreen(void);
+    void Resize(int, int);
+    void UnInitialize(void);
+
+    //code
+    switch(iMsg)
+    {              
+        case WM_SETFOCUS:
+            gbActiveWindow = true;
+            break;
+        
+        case WM_KILLFOCUS:
+            gbActiveWindow = false;
+            break;
+
+        case WM_ERASEBKGND:
+            return (0);
+
+        case WM_SIZE:
+            Resize(LOWORD(lParam), HIWORD(lParam));
+            break;
+
+        case WM_KEYDOWN:
+            switch(wParam)
+            {
+                case VK_ESCAPE:
+                    DestroyWindow(hwnd);
+                    break;
+
+                case 0x46:
+                    //fall through
+                case 0x66:
+                    ToggleFullscreen();
+                    break;
+                
+                default:
+                    break;
+            }
+            break;
+
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+            break;
+
+        case WM_DESTROY:
+            UnInitialize();
+            PostQuitMessage(0);
+            break;
+        
+        default:
+            break;
+    }
+
+    return (DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+
+//ToggleFullscreen() - toggle fullscreen
+void ToggleFullscreen(void)
+{
+    //variable declaration
+    MONITORINFO mi = { sizeof(MONITORINFO) };
+
+    //code
+    if(gbFullscreen == false)
+    {
+        dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+        if(dwStyle & WS_OVERLAPPEDWINDOW)
+        {
+            if(GetWindowPlacement(ghwnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+            {
+                SetWindowLong(ghwnd, GWL_STYLE, (dwStyle & ~WS_OVERLAPPEDWINDOW));
+                SetWindowPos(ghwnd,
+                    HWND_TOP,
+                    mi.rcMonitor.left,
+                    mi.rcMonitor.top,
+                    mi.rcMonitor.right - mi.rcMonitor.left,
+                    mi.rcMonitor.bottom - mi.rcMonitor.top,
+                    SWP_NOZORDER | SWP_FRAMECHANGED);
+            }
+        }
+
+        ShowCursor(false);
+        gbFullscreen = true;
+    }
+    else
+    {
+        SetWindowLong(ghwnd, GWL_STYLE, (dwStyle | WS_OVERLAPPEDWINDOW));
+        SetWindowPlacement(ghwnd, &wpPrev);
+        SetWindowPos(ghwnd,
+            HWND_TOP,
+            0,
+            0,
+            0,
+            0,
+            SWP_NOOWNERZORDER | SWP_NOSIZE | SWP_NOMOVE | SWP_NOZORDER | SWP_FRAMECHANGED);
+    
+        ShowCursor(true);
+        gbFullscreen = false;
+    }
+    
+}
+
+
+//Initialize() - initializes rendering context
+void Initialize(void)
+{
+    //function prototypes
+    void Resize(int, int);
+
+    //variable declaration
+    PIXELFORMATDESCRIPTOR pfd;
+    int iPixelFormatIndex;
+
+    //code
+    ghdc = GetDC(ghwnd);
+
+    ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+    //initialization of PIXELFORMATDESCRIPTOR
+    pfd.nSize       = sizeof(PIXELFORMATDESCRIPTOR);
+    pfd.nVersion    = 1;
+    pfd.dwFlags     = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType  = PFD_TYPE_RGBA;
+    pfd.cColorBits  = 32;
+    pfd.cRedBits    = 8;
+    pfd.cBlueBits   = 8;
+    pfd.cGreenBits  = 8;
+    pfd.cAlphaBits  = 8;
+    //pfd.cDepthBits  = 32;
+
+    //choose required pixel format from device context
+    iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+    if(iPixelFormatIndex == 0)
+    {
+        fprintf(gpFile, "ChoosePixelFormat() failed.\n");
+        DestroyWindow(ghwnd);
+    }
+
+    //set that pixel format as current
+    if(SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE)
+    {
+        fprintf(gpFile, "SetPixelFormat() failed.\n");
+        DestroyWindow(ghwnd);
+    }
+
+    ghrc = wglCreateContext(ghdc);
+    if(ghrc == NULL)
+    {
+        fprintf(gpFile, "wglCreateContext() failed.\n");
+        DestroyWindow(ghwnd);
+    }
+
+    if(wglMakeCurrent(ghdc, ghrc) == FALSE)
+    {
+        fprintf(gpFile, "wglMakeCurrent() failed.\n");
+        DestroyWindow(ghwnd);
+    }
+
+    //warm-up call to Resize()
+    Resize(WIN_WIDTH, WIN_HEIGHT);
+
+    //set clearing color
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+ 
+}
+
+
+//Resize() 
+void Resize(int width, int height)
+{
+    //code
+    if(height == 0)
+        height = 1;
+
+    glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+}
+
+
+//Display() - renders scene
+void Display(void)
+{
+    //variable declaration
+    float x, y, fAngle, radius;
+    float step = 0.0005f;
+   
+    static GLfloat Tcx = -5.0f; 
+    static GLfloat Tcy = -5.0f;
+    
+    static GLfloat Twy = 5.0f;
+
+    static GLfloat Tsx = 5.0f;
+    static GLfloat Tsy = -5.0f;
+
+    bool cloak = true;
+    bool wand = false;
+    bool stone = false;
+
+    //code
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();    
+
+    glLineWidth(4);
+
+    //invisibility cloak
+    if(cloak)
+    {
+        glTranslatef(Tcx, Tcy, -3.0f);
+        glScalef(0.5f, 0.5f, 0.5f);
+        glBegin(GL_LINE_LOOP);
+            glVertex3f(0.0f, 1.0f, 0.0f);
+            glVertex3f(-1.0f, -1.0f, 0.0f);
+            glVertex3f(1.0f, -1.0f, 0.0f);
+        glEnd();
+
+        if(Tcx >= 0.0f)
+            stone = true;
+    }
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity(); 
+
+    //resurrection stone
+    if(stone)
+    {
+        //trnslate to center of incircle
+        glTranslatef(Tsx, Tsy - 0.19f, -3.0f);
+        glScalef(0.5f, 0.5f, 0.5f);
+        
+        //radius of incircle = area / semi-perimeter;
+        radius = 2.0f / 3.236f;
+        
+        glBegin(GL_LINE_LOOP);
+            for(fAngle = 0.0f; fAngle < 2 * PI; fAngle = fAngle + 0.05f)
+            {
+                x = radius * sin(fAngle);
+                y = radius * cos(fAngle);
+
+                glVertex3f(x, y, 0.0f);
+            }
+        glEnd();
+
+        if(Tsx <= 0.0f)
+            wand = true;
+    }
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity(); 
+
+    //elder wand
+    if(wand)
+    {
+        glTranslatef(0.0f, Twy, -3.0f);
+        glScalef(0.5f, 0.5f, 0.5f);
+
+        glBegin(GL_LINES);
+            glVertex3f(0.0f, 1.0f, 0.0f);
+            glVertex3f(0.0f, -1.0f, 0.0f);
+        glEnd();
+    }
+
+    //Update
+    if(cloak)
+    {
+        if(Tcx < 0.0f)
+            Tcx = Tcx + step;
+
+        if(Tcy < 0.0f)
+            Tcy = Tcy + step;    
+    }
+
+    if(wand)
+    {
+        if(Twy > 0.0f)
+            Twy = Twy - step;
+    }
+
+    if(stone)
+    {
+        if(Tsx > 0.0f)
+            Tsx = Tsx - step;
+
+        if(Tsy < 0.0f)
+            Tsy = Tsy + step;  
+    }
+
+    SwapBuffers(ghdc);
+}
+
+
+//UnInitialize()
+void UnInitialize(void)
+{
+    //code
+    if(gbFullscreen == true)
+    {
+        dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+        SetWindowLong(ghwnd, GWL_STYLE, (dwStyle | WS_OVERLAPPEDWINDOW));
+        SetWindowPlacement(ghwnd, &wpPrev);
+        SetWindowPos(ghwnd,
+            HWND_TOP,
+            0,
+            0,
+            0,
+            0,
+            SWP_NOOWNERZORDER | SWP_NOSIZE | SWP_NOMOVE | SWP_NOZORDER | SWP_FRAMECHANGED);
+    
+        ShowCursor(true);
+    }
+
+    if(wglGetCurrentContext() == ghrc)
+    {
+        wglMakeCurrent(NULL, NULL);
+    }
+
+    if(ghrc)
+    {
+        wglDeleteContext(ghrc);
+        ghrc = NULL;
+    }
+
+    if(ghdc)
+    {
+        ReleaseDC(ghwnd, ghdc);
+        ghdc = NULL;
+    }
+
+    if(gpFile)
+    {
+        fprintf(gpFile, "\nProgram completed successfully.\n'log.txt' file closed successfully.\n");
+        fclose(gpFile);
+        gpFile = NULL;
+    }
+}
